73c6ebe5f55c5e93a4396bebcb5eeb3b9062bebb
anurag-dwivedi-ttn <anurag.dwivedi@tothenew.com>
2025-02-28 23:09:37 +0530

    Maven-Assignment Completed with given instructions

---
+++ SubClass1/src/main/java/com/ttn/SubClass1/App.java
@@ -0,0 +1,15 @@
+package com.ttn.SubClass1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    // Ques-4
+    // Executed using the Jar Plugin
+    public static void main( String[] args )
+    {
+        System.out.println("Hello from SubClass 1");
+    }
+}
---
+++ SubClass2/src/main/java/com/ttn/SubClass2/App.java
@@ -0,0 +1,11 @@
+package com.ttn.SubClass2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from SubClass2");
+    }
+}
